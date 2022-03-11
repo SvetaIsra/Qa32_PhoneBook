c875aecb0cca93bd67481ebe6bb34c8ca970180b
SvetaIsra <svetlana.osypenko.il@gmail.com>
2022-03-11 12:03:07 +0200

    commit

---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+    @Test
+    public void registrationSuccess(){
+//open regform
+        //fill email
+        //fill password
+        //click registr
+    }
+}
